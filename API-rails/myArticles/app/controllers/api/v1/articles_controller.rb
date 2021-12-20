ed9bb6378ed343a00d5f45dc51bfdd9c5638e005
luana saraiva <luana_saraiva2@hotmail.com>
2021-12-20 14:58:14 -0300

    index and show - CRUD

--- API-rails/myArticles/app/controllers/api/v1/articles_controller.rb
+++ API-rails/myArticles/app/controllers/api/v1/articles_controller.rb
@@ -1,10 +1,15 @@
 module Api
   module V1
     class ArticlesController < ApplicationController
-      #lista todos os artigos por ordem decrescente 
-      def index
-        articles = Article.order('created_at DESC')
-        render json: {status: 'success', message: 'Articles sucessfully created', data: articles} status: :ok
+      #lista todos os artigos por ordem ascendente(default) limitando pelos 5 primeiros
+      def index #localhost:3000/api/v1/articles
+        articles = Article.order('created_at LIMIT 5')
+        render json: {status: 'success', message: 'Articles sucessfully created', data: articles}, status: :ok
+      end
+
+      def show #localhost:3000/api/v1/articles/4
+        article = Article.find(params[:id])
+        render json: {status: 'success', message: 'Here is your Article :)', data: article}, status: :ok
       end
     end
   end
