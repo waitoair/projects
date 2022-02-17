0c9520f71e2162e7730ef014f36f089dd1cda455
luana saraiva <luana_saraiva2@hotmail.com>
2022-02-17 20:40:22 -0300

    some changes in formValidator

--- Form Validator/script.js
+++ Form Validator/script.js
@@ -1,12 +1,19 @@
-let b7validator = {
-  handleSubmit(event) {
+let b7Validator = {
+  handleSubmit:(event)=>{
     event.preventDefault();
     let send = true;
 
     let inputs = form.querySelectorAll('input');
-    for (let i = 0; i < inputs.length; i++) {
-      let input = inputs[i]
-      console.log(input);
+
+    // b7Validator.clearErrors();
+
+    for(let i=0;i<inputs.length;i++) {
+      let input = inputs[i];
+      let check = b7Validator.checkInput(input);
+      if(check !== true) {
+        send = false;
+        b7Validator.showError(input, check);
+      }
     }
 
     if(send) {
@@ -16,4 +23,4 @@ let b7validator = {
 }
 
 let form = document.querySelector('.b7validator');
-form.addEventListener('submit', b7validator.handleSubmit)
+form.addEventListener('submit', B7Validator.handleSubmit);
