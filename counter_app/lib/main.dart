d598dc0eaabb4c48cf15e4b7f57e2d54c7cab927
Luana Saraiva <luana_saraiva2@hotmail.com>
2022-06-08 10:08:14 -0300

    counter flutter project

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -2,15 +2,41 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-          color: Colors.purple,
-          child: Container(
-            height: 200,
-            width: 200,
-            color: Colors.greenAccent,
-          )
+    theme: ThemeData(primarySwatch: Colors.purple),
+    home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment(){
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Meu App Contador"),
       ),
-    )
-  ));
+      body: Center(
+        child: Text(
+          "Contador\n$count",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: (){
+          increment();
+        },
+      ),
+    );
+  }
 }
